e46662f12b8e7fe58a8a6c78a881157ffb54a560
Negr087 <spibanezpaypal@gmail.com>
2025-02-10 20:22:11 -0300

    Changes to be committed

--- components/NostrClient.tsx
+++ components/NostrClient.tsx
@@ -24,6 +24,21 @@ export default function NostrClient() {
     "wss://nos.lol",
     "wss://relay.snort.social",
   ];
+  // Define el componente GitHubLogo
+  const GitHubLogo = () => (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+    </svg>
+  );
 
   // Función para restablecer el estado inicial
   const resetState = () => {
@@ -293,6 +308,16 @@ export default function NostrClient() {
           alt="Logo"
           className="ml-2 h-6 w-6" // Ajusta el tamaño según sea necesario
         />
+        {/* Agrega el ícono de GitHub aquí */}
+        <a
+          href="https://github.com/Negr087/Nostr0" // Reemplaza con la URL de tu repositorio
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-violet-400 hover:text-pink-400 transition-colors"
+        >
+          <GitHubLogo />
+        </a>
+        <span className="ml-2 text-yellow-400"></span>
         <span className="ml-2 text-yellow-400"></span>
       </div>
     </div>
